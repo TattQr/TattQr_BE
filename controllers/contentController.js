17900f3274a38df9fbe5d139bc6a4c831b40b939
TattQr <tattqr1@gmail.com>
2025-02-05 03:53:31 +0500

    changes made for temp and secure contents

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -98,7 +98,7 @@ console.log("req.query is", req.query);
       qrCode: qrId,
       contentType: contentType ? contentType : "text",
       contentUrl: contentUrl
-        ? `https://damonbe-production-ff33.up.railway.app${contentUrl}`
+        ? `https://tattqrbe-production.up.railway.app${contentUrl}`
           // `http://localhost:5000${contentUrl}`
         : null,
       text: text ? text : "",
